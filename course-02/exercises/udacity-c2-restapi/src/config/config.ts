1c838c9d795a552fc3a9982878728ad37a8e4350
Patrik Björk <patrik.bjork@codespirit.se>
2020-04-25 18:16:07 +0200

    Unstub password generation and comparison methods. Unstub requireAuth method. Test password generation and comparison. Test JWT token. Refactor config.ts.

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,17 @@
 export const config = {
-  "dev": {
-    "username": process.env.UDAGRAM_POSTGRES_USER,
-    "password": process.env.UDAGRAM_POSTGRES_PASSWORD,
-    "database": process.env.UDAGRAM_POSTGRES_DATABASE,
-    "host": process.env.UDAGRAM_POSTGRES_HOST,
-    "dialect": "postgres",
-    "aws_region": process.env.UDAGRAM_AWS_REGION,
-    "aws_profile": process.env.UDAGRAM_AWS_PROFILE,
-    "aws_media_bucket": process.env.UDAGRAM_AWS_MEDIA_BUCKET
+  'postgres': {
+    'username': process.env.UDAGRAM_POSTGRES_USER,
+    'password': process.env.UDAGRAM_POSTGRES_PASSWORD,
+    'database': process.env.UDAGRAM_POSTGRES_DATABASE,
+    'host': process.env.UDAGRAM_POSTGRES_HOST,
+    'dialect': 'postgres'
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+  'aws': {
+    'aws_region': process.env.UDAGRAM_AWS_REGION,
+    'aws_profile': process.env.UDAGRAM_AWS_PROFILE,
+    'aws_media_bucket': process.env.UDAGRAM_AWS_MEDIA_BUCKET
+  },
+  'jwt': {
+    'secret': 'LAKSJDFalkfdjaslkdfj'
   }
-}
+};
